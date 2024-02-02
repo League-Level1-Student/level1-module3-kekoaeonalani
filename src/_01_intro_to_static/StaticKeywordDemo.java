5f7325f59092f41a3561560ffe503b6529b97cad
league <league@192.168.1.85>
2024-02-01 19:59:45 -0800

    continue intro to static step 9-10

--- src/_01_intro_to_static/StaticKeywordDemo.java
+++ src/_01_intro_to_static/StaticKeywordDemo.java
@@ -67,14 +67,18 @@ public class StaticKeywordDemo {
         addCircles();
     }
     
-    void addCircles() {
+    static void addCircles() {
+    	
         
         Circle circle1 = new Circle(20, Color.BLUE);
-        Circle circle2 = new Circle(50, Color.RED);
+        Circle circle2 = new Circle(50, Color.GREEN);
         Circle circle3 = new Circle(100, Color.GREEN);
         
-        canvas.addCircle(circle1);
-        canvas.addCircle(circle2);
-        canvas.addCircle(circle3);
+        System.out.println("circle radius = " + circle1.radius);
+   	 System.out.println("circle color = " + circle1.color);
+        
+        static canvas.addCircle(circle1);
+        static canvas.addCircle(circle2);
+        static canvas.addCircle(circle3);
     }
 }
